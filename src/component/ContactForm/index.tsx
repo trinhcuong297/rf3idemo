87fe036ffad1af3940bb741b9d50000ed35c81cb
trinhcuong297 <trinhcuong297@gmail.com>
2022-10-19 18:59:35 +0700

    addContactPage_new

--- src/component/ContactForm/index.tsx
+++ src/component/ContactForm/index.tsx
@@ -1,12 +1,12 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
 import { FaRegEnvelope, FaPaperPlane, FaCheckCircle } from 'react-icons/fa';
 
-import { phoneMask } from '../../utils/mask';
+// import { phoneMask } from '../../utils/mask';
 import Input from '../Input';
 import {
   Container,
-  Loading,
+  // Loading,
   Title,
   TitleLines,
   GridInputs,
@@ -15,38 +15,42 @@ import {
 } from './styles';
 
 const ContactForm: React.FC = () => {
-  const [phone, setPhone] = useState('');
-  const [loading, setLoading] = useState(false);
+  // const [phone, setPhone] = useState('');
+  // const [loading, setLoading] = useState(false);
   const [successSubmit, setSuccessSubmit] = useState(false);
 
-  const handleSubmit = useCallback(
-    (event) => {
-      event.preventDefault();
-      const data = {
-        name: event.target[0].value,
-        email: event.target[1].value,
-        phone,
-        company: event.target[3].value,
-        message: event.target[4].value,
-      };
-      setLoading(true);
-      fetch('/api/contact', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-      }).then((res) => {
-        if (res.ok) {
-          setLoading(false);
-          setSuccessSubmit(true);
-        }
-      });
-    },
-    [phone]
-  );
+  // const handleSubmit = useCallback(
+  //   (event) => {
+  //     event.preventDefault();
+  //     const data = {
+  //       name: event.target[0].value,
+  //       email: event.target[1].value,
+  //       phone,
+  //       company: event.target[3].value,
+  //       message: event.target[4].value,
+  //     };
+  //     setLoading(true);
+  //     fetch('/api/contact', {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify(data),
+  //     }).then((res) => {
+  //       if (res.ok) {
+  //         setLoading(false);
+  //         setSuccessSubmit(true);
+  //       }
+  //     });
+  //   },
+  // [phone]
+  // );
+
+  const sucSub = () => {
+    setSuccessSubmit(true);
+  };
 
   return (
     <Container>
-      {loading && <Loading />}
+      {/* {loading && <Loading />} */}
 
       <Title>
         <h1 className="text-green-600 text-3xl">SEND RF3I A MESSAGE</h1>
@@ -60,7 +64,10 @@ const ContactForm: React.FC = () => {
         </div>
       </Title>
       {!successSubmit && (
-        <form onSubmit={handleSubmit}>
+        <form
+          // onSubmit={handleSubmit}
+          onSubmit={sucSub}
+        >
           <GridInputs>
             <Input
               name="name"
@@ -81,9 +88,9 @@ const ContactForm: React.FC = () => {
               label="Phone number"
               type="text"
               placeholder="(+84)"
-              value={phone}
+              // value={phone}
               maxLength={15}
-              onChange={(e) => setPhone(phoneMask(e.target.value))}
+              // onChange={(e) => setPhone(phoneMask(e.target.value))}
               style={{ outline: 'none' }}
             />
             <Input
