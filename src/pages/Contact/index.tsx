720fd918f83d1abb1b8210650028056b40ec953c
trinhcuong297 <trinhcuong297@gmail.com>
2022-10-20 01:41:41 +0700

    addContactPage_new

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
+import Head from 'next/head';
+
 import ContactForm from '../../component/ContactForm';
 import ContactInformation from '../../component/ContactInformation';
 import { Container, Content } from '../../styles/Home';
 
 const ContactUs: React.FC = () => (
-  <Container className="bg-gray-100">
-    <Content>
-      <ContactForm />
-      <ContactInformation />
-    </Content>
-  </Container>
+  <>
+    <Head>
+      <title>RF3i - Contact Us</title>
+    </Head>
+    <Container className="bg-gray-100">
+      <Content>
+        <ContactForm />
+        <ContactInformation />
+      </Content>
+    </Container>
+  </>
 );
 
 export default ContactUs;
