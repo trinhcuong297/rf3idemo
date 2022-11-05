3ec7548e0e23972cc3dc0cd04f9d0853f24033cf
trinhcuong297 <trinhcuong297@gmail.com>
2022-11-05 09:05:42 +0700

    changeHomeAndFooter

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -89,7 +89,7 @@ const Footer = () => (
   //   </Section>
   // </Background>
   <>
-    <footer className="py-6 dark:bg-gray-800 dark:text-gray-50 mt-6">
+    <footer className="py-6 bg-gray-800 text-gray-50 mt-6">
       <div className="container px-6 md:px-12 mx-auto space-y-6 divide-y divide-gray-400 md:space-y-12 divide-opacity-50">
         <div className="grid grid-cols-12 gap-6">
           <div className="pb-6 col-span-full md:pb-0 md:col-span-6 flex flex-col items-center md:items-start">
@@ -98,8 +98,8 @@ const Footer = () => (
               href="#"
               className="flex justify-center space-x-3 md:justify-start"
             >
-              {/* <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-400">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-5 h-5 rounded-full dark:text-gray-900">
+              {/* <div className="flex items-center justify-center w-12 h-12 rounded-full bg-violet-400">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-5 h-5 rounded-full text-gray-900">
 								<path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
 							</svg>
 						</div>
@@ -115,7 +115,7 @@ const Footer = () => (
                 <a
                   rel="noopener noreferrer"
                   href="#"
-                  className="hover:dark:text-violet-400"
+                  className="hover:text-violet-400"
                 >
                   Link
                 </a>
@@ -124,7 +124,7 @@ const Footer = () => (
                 <a
                   rel="noopener noreferrer"
                   href="#"
-                  className="hover:dark:text-violet-400"
+                  className="hover:text-violet-400"
                 >
                   Link
                 </a>
@@ -133,7 +133,7 @@ const Footer = () => (
                 <a
                   rel="noopener noreferrer"
                   href="#"
-                  className="hover:dark:text-violet-400"
+                  className="hover:text-violet-400"
                 >
                   Link
                 </a>
@@ -142,7 +142,7 @@ const Footer = () => (
                 <a
                   rel="noopener noreferrer"
                   href="#"
-                  className="hover:dark:text-violet-400"
+                  className="hover:text-violet-400"
                 >
                   Link
                 </a>
@@ -151,57 +151,7 @@ const Footer = () => (
                 <a
                   rel="noopener noreferrer"
                   href="#"
-                  className="hover:dark:text-violet-400"
-                >
-                  Link
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="text-center col-span-4 sm:col-span-6 md:text-left md:col-span-2">
-            <p className="pb-1 text-lg font-medium">Category</p>
-            <ul>
-              <li>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  className="hover:dark:text-violet-400"
-                >
-                  Link
-                </a>
-              </li>
-              <li>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  className="hover:dark:text-violet-400"
-                >
-                  Link
-                </a>
-              </li>
-              <li>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  className="hover:dark:text-violet-400"
-                >
-                  Link
-                </a>
-              </li>
-              <li>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  className="hover:dark:text-violet-400"
-                >
-                  Link
-                </a>
-              </li>
-              <li>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  className="hover:dark:text-violet-400"
+                  className="hover:text-violet-400"
                 >
                   Link
                 </a>
@@ -215,7 +165,7 @@ const Footer = () => (
                 <a
                   rel="noopener noreferrer"
                   href="#"
-                  className="hover:dark:text-violet-400"
+                  className="hover:text-violet-400"
                 >
                   Link
                 </a>
@@ -224,7 +174,7 @@ const Footer = () => (
                 <a
                   rel="noopener noreferrer"
                   href="#"
-                  className="hover:dark:text-violet-400"
+                  className="hover:text-violet-400"
                 >
                   Link
                 </a>
@@ -233,7 +183,7 @@ const Footer = () => (
                 <a
                   rel="noopener noreferrer"
                   href="#"
-                  className="hover:dark:text-violet-400"
+                  className="hover:text-violet-400"
                 >
                   Link
                 </a>
@@ -242,7 +192,7 @@ const Footer = () => (
                 <a
                   rel="noopener noreferrer"
                   href="#"
-                  className="hover:dark:text-violet-400"
+                  className="hover:text-violet-400"
                 >
                   Link
                 </a>
@@ -251,7 +201,57 @@ const Footer = () => (
                 <a
                   rel="noopener noreferrer"
                   href="#"
-                  className="hover:dark:text-violet-400"
+                  className="hover:text-violet-400"
+                >
+                  Link
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="text-center col-span-4 sm:col-span-6 md:text-left md:col-span-2">
+            <p className="pb-1 text-lg font-medium">Category</p>
+            <ul>
+              <li>
+                <a
+                  rel="noopener noreferrer"
+                  href="#"
+                  className="hover:text-violet-400"
+                >
+                  Link
+                </a>
+              </li>
+              <li>
+                <a
+                  rel="noopener noreferrer"
+                  href="#"
+                  className="hover:text-violet-400"
+                >
+                  Link
+                </a>
+              </li>
+              <li>
+                <a
+                  rel="noopener noreferrer"
+                  href="#"
+                  className="hover:text-violet-400"
+                >
+                  Link
+                </a>
+              </li>
+              <li>
+                <a
+                  rel="noopener noreferrer"
+                  href="#"
+                  className="hover:text-violet-400"
+                >
+                  Link
+                </a>
+              </li>
+              <li>
+                <a
+                  rel="noopener noreferrer"
+                  href="#"
+                  className="hover:text-violet-400"
                 >
                   Link
                 </a>
@@ -274,7 +274,7 @@ const Footer = () => (
           <a
             rel="noopener noreferrer"
             href="#"
-            className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-violet-400 dark:text-gray-900"
+            className="flex items-center justify-center w-10 h-10 rounded-full bg-violet-400 text-gray-900"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -289,7 +289,7 @@ const Footer = () => (
           <a
             rel="noopener noreferrer"
             href="#"
-            className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-violet-400 dark:text-gray-900"
+            className="flex items-center justify-center w-10 h-10 rounded-full bg-violet-400 text-gray-900"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -303,7 +303,7 @@ const Footer = () => (
           <a
             rel="noopener noreferrer"
             href="#"
-            className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-violet-400 dark:text-gray-900"
+            className="flex items-center justify-center w-10 h-10 rounded-full bg-violet-400 text-gray-900"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
