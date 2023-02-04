03564dccbb490fccc037d263c82313c6b3e4ccff
trinhcuong297 <trinhcuong297@gmail.com>
2023-02-05 06:12:48 +0700

    getApiPublication

--- src/pages/Publication.tsx
+++ src/pages/Publication.tsx
@@ -1,7 +1,61 @@
+import { useState, useEffect } from 'react';
+
 import Head from 'next/head';
 import Link from 'next/link';
 
 export default function Publication() {
+  const [data, setData] = useState<Data[]>([]);
+  const [isLoading, setLoading] = useState(false);
+
+  interface Data {
+    title?: String | null | undefined;
+    authors?: String | null | undefined;
+    publisher?: String | null | undefined;
+    published_in?: String | null | undefined;
+    published_date?: String | null | undefined;
+    url?: String | null | undefined;
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `${process.env.RF3i_API}/rf3i-api/publication/all-publication?page=1&size=8`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data);
+        setData(data.data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading)
+    return (
+      <section id="ourMember" className=" bg-gray-200 rounded-2xl p-4 m-4">
+        <div className="flex items-center justify-center mt-6">
+          <h2 className="text-4xl font-extrabold pt-4 text-transparent bg-clip-text bg-gradient-to-tr from-blue-500 to-blue-400">
+            RF3i Publication
+          </h2>
+        </div>
+        <p className="w-full flex justify-center items-center">
+          <div className="relative w-20 h-20 animate-pulse rounded-full bg-gradient-to-r from-purple-400 via-blue-500 to-red-400 ">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-28 h-28 bg-gray-200 rounded-full border-2 border-white">
+              <img src="https://img.icons8.com/clouds/2x/group.png" alt="" />
+            </div>
+          </div>
+        </p>
+      </section>
+    );
+  if (!data)
+    return (
+      <section id="ourMember" className=" bg-gray-200 rounded-2xl p-4 m-4">
+        <div className="flex items-center justify-center mt-6">
+          <h2 className="text-4xl font-extrabold pt-4 text-transparent bg-clip-text bg-gradient-to-tr from-blue-500 to-blue-400">
+            RF3i Publication
+          </h2>
+        </div>
+      </section>
+    );
   return (
     <>
       <Head>
@@ -25,58 +79,66 @@ export default function Publication() {
               <table className="w-full border border-collapse table-auto">
                 <thead className="">
                   <tr className="text-base font-bold text-left bg-gray-50 grid grid-cols-12">
-                    <th className="text-gray-700 px-4 py-3 border-b-2 border-blue-500 col-span-6">
+                    <th className="text-gray-700 px-4 py-3 border-b-2 border-blue-500 col-span-4">
                       Title
                     </th>
                     <th className="text-gray-700 px-4 py-3 text-center border-b-2 border-sky-500 sm:text-left col-span-3">
-                      Public by
+                      Author
                     </th>
                     <th className="text-gray-700 px-4 py-3 text-center border-b-2 border-cyan-500 sm:text-left col-span-2">
-                      Public time
+                      Published in
+                    </th>
+                    <th className="text-gray-700 px-4 py-3 text-center border-b-2 border-cyan-500 sm:text-left col-span-2">
+                      Published Date
                     </th>
                     <th className="text-gray-700 px-4 py-3 text-center border-b-2 border-cyan-500 sm:text-left col-span-1">
                       Dowload
                     </th>
                   </tr>
                 </thead>
-                <tbody className="text-sm font-normal text-gray-700">
-                  <tr className="py-1 border-b border-gray-200 hover:bg-gray-100 grid grid-cols-12">
-                    <td className="flex flex-row items-center px-4 col-span-6">
-                      <div className="flex mr-4">
-                        <Link href={'/'}>
-                          <p className="cursor-pointer relative blocktext-md">
-                            abc
-                          </p>
-                        </Link>
-                      </div>
-                    </td>
-                    <td className="flex flex-row items-center sm:text-left px-4 col-span-3">
-                      <div className="flex-1 pl-1 sm:text-left">
-                        <div className="font-medium">Barbara Curtis</div>
-                      </div>
-                    </td>
-                    <td className="flex flex-row items-center px-4 text-center sm:text-left col-span-2">
-                      Just Now
-                    </td>
-                    <td className="px-4 text-center sm:text-left col-span-1">
-                      <Link href={'https://icons8.com/icon/set/link/clouds'}>
-                        <a target="_blank" rel="noopener noreferrer">
-                          <img
-                            src="https://img.icons8.com/clouds/2x/external-link.png"
-                            className="w-10 h-10 cursor-pointer"
-                          />
-                        </a>
-                      </Link>
-                    </td>
-                  </tr>
-                </tbody>
+
+                {data.map((param, key) => {
+                  return (
+                    <tbody
+                      className="text-sm font-normal text-gray-700"
+                      key={key}
+                    >
+                      <tr className="py-1 border-b border-gray-200 hover:bg-gray-100 grid grid-cols-12">
+                        <td className="flex flex-row items-center px-4 py-2 col-span-4">
+                          <p className="cursor-pointer">{param.title}</p>
+                        </td>
+                        <td className="flex flex-row items-center sm:text-left px-4 col-span-3">
+                          <div className="flex-1 sm:text-left">
+                            <div className="font-medium">{param.authors}</div>
+                          </div>
+                        </td>
+                        <td className="flex flex-row items-center px-4 text-center sm:text-left col-span-2">
+                          {param.published_in}
+                        </td>
+                        <td className="flex flex-row items-center px-4 text-center sm:text-left col-span-2">
+                          {param.published_date}
+                        </td>
+                        <td className="flex items-center px-4 text-center sm:text-left col-span-1">
+                          <Link href={`${param.url}`}>
+                            <a target="_blank" rel="noopener noreferrer">
+                              <img
+                                src="https://img.icons8.com/clouds/2x/external-link.png"
+                                className="w-10 h-10 cursor-pointer"
+                              />
+                            </a>
+                          </Link>
+                        </td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
               </table>
             </div>
             <div className="flex flex-col items-center w-full px-4 py-2 space-y-2 text-sm text-gray-500 sm:justify-between sm:space-y-0 sm:flex-row">
-              <p className="flex">
+              {/* <p className="flex">
                 Showing&nbsp;<span className="font-bold"> 1 to 4 </span>&nbsp;of
                 8 entries
-              </p>
+              </p> */}
               {/* <div className="flex items-center justify-between space-x-2">
       <a href="#" className="hover:text-gray-600">Previous</a>
       <div className="flex flex-row space-x-1">
