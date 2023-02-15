9e9f2094e1042415f6c61e08f58a68e7c38ed15e
trinhcuong297 <trinhcuong297@gmail.com>
2023-02-15 21:12:16 +0700

    addSlider

--- src/Home/Hero.tsx
+++ src/Home/Hero.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Link from 'next/link';
-import 'tw-elements';
 
 function Hero() {
   return (
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,26 @@
 import { AppProps } from 'next/app';
 import NextNProgress from 'nextjs-progressbar';
 
+import 'tw-elements'; // @ts-ignore
 import { Footer } from '../Home/Footer';
 import Navbar from '../Navbar/navbar';
 import '../styles/global.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <NextNProgress options={{ showSpinner: false }} />
-    <div className="bg-gray-100">
-      <div className="z-10 fixed w-screen top-0">
-        <Navbar />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <NextNProgress options={{ showSpinner: false }} />
+      <div className="bg-gray-100">
+        <div className="z-10 fixed w-screen top-0">
+          <Navbar />
+        </div>
+        <div className="bg-gray-100 pt-[3.5rem]">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      {/* <div className="fixed left-0 hidden md:block z-10">
-        <Sidebar />
-      </div> */}
-      <div className="bg-gray-100 pt-[3.5rem]">
-        <Component {...pageProps} />
-      </div>
-      <Footer />
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default MyApp;
