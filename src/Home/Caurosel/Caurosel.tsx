12fc4e698bdcde8bc54a089f66208eae0f3ff9da
trinhcuong297 <trinhcuong297@gmail.com>
2023-02-18 12:29:21 +0700

    addNewSlider

--- src/Home/Caurosel/Caurosel.tsx
+++ src/Home/Caurosel/Caurosel.tsx
@@ -1,35 +1,69 @@
-import { EffectFade, Navigation, Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from 'react';
 
+import { Autoplay, Pagination, Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 export default function Caurosel() {
   return (
     <>
       <Swiper
         spaceBetween={30}
-        effect={'fade'}
-        navigation={true}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: true,
+        }}
         pagination={{
           clickable: true,
         }}
-        modules={[EffectFade, Navigation, Pagination]}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          <img
+            className="w-full h-screen"
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+          <img
+            className="w-full h-screen"
+            src="https://swiperjs.com/demos/images/nature-2.jpg"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+          <img
+            className="w-full h-screen"
+            src="https://swiperjs.com/demos/images/nature-3.jpg"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+          <img
+            className="w-full h-screen"
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="w-full h-screen"
+            src="https://swiperjs.com/demos/images/nature-5.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="w-full h-screen"
+            src="https://swiperjs.com/demos/images/nature-6.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="w-full h-screen"
+            src="https://swiperjs.com/demos/images/nature-7.jpg"
+          />
         </SwiperSlide>
       </Swiper>
     </>
